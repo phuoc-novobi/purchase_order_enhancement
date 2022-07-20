f8b424e1357d1bed7eccce5a7b6bca9e4ab2b2d7
phuoc-novobi <phuoc.doan@novobi.com>
2022-07-20 14:26:45 +0700

    Add models & views

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Purchase Order Enhancement",
+    'summary': "Enhance your purchase orders",
+    # 'description': """
+    #     Manage Library
+    #     ==============
+    #     Description related to library.
+    #     """,
+    'author': "Phuoc Doan",
+    'category': 'Tools',
+    'version': '15.0',
+    'depends': ['base', 'purchase'],
+    'data': [
+        'views/purchase_views.xml',
+    ],
+    'application': True,
+    # 'demo': ['demo.xml'],
+}
---
+++ models/purchase.py
@@ -0,0 +1,25 @@
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    active = fields.Boolean(
+        'Is Active?',
+        default=True,
+        readonly=True,
+        states={'done': [('readonly', False)], 'cancel': [('readonly', False)]}
+    )
+
+    def button_archive(self):
+        if self.state in ['cancel', 'done']:
+            self.write({'active': False})
+        return {}
+
+    def action_archive(self):
+        if len(self.filtered(lambda record: record.state not in ['done', 'cancel'])) > 0:
+            raise UserError(
+                'You cannot archive Purchase Orders that are not done/cancelled!')
+        for record in self.filtered(lambda record: record.state in ['done', 'cancel']):
+            record.write({'active': False})
