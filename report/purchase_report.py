7fcb9dd55d02c28ea35f325a85d9fa49670e6f5e
phuoc-novobi <phuoc.doan@novobi.com>
2022-07-22 10:03:17 +0700

    Add pivot view

--- report/purchase_report.py
+++ report/purchase_report.py
@@ -5,10 +5,11 @@ from odoo import api, fields, models
 class PurchaseReport(models.Model):
     _inherit = 'purchase.report'
 
-    payment_term_id = fields.Many2one('purchase.order', 'Payment Term')
+    payment_term_id = fields.Many2one('account.payment.term', 'Payment Terms',
+                                      domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
 
     def _select(self):
-        return super(PurchaseReport, self)._select() + 'po.payment_term_id as payment_term_id'
+        return super(PurchaseReport, self)._select() + ', po.payment_term_id as payment_term_id'
 
     def _group_by(self):
-        return super(PurchaseReport, self)._group_by() + 'po.payment_term_id'
+        return super(PurchaseReport, self)._group_by() + ', po.payment_term_id'
